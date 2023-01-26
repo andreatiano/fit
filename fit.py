ed909c333483a50c708f3820e89217204572bf81
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-01-26 16:20:33 +0100

    Update fit.py

--- fit.py
+++ fit.py
@@ -60,6 +60,6 @@ if fileName is not None:
     specraPlot.pyplot(fig)
 
     plotData=dataCol.expander('Interpolated data',True)
-    plotDataFrame=pd.DataFrame(np.vstack((xnew,nfit(xnew),kfit(xnew))).transpose(),columns = ['wavelenght','n','k'])
+    plotDataFrame=pd.DataFrame(np.vstack((xnew.reverse,nfit(xnew).reverse,kfit(xnew).reverse)).transpose(),columns = ['wavelenght','n','k'])
     plotData.dataframe(plotDataFrame) 
     plotData.download_button('Download current dispersion data',plotDataFrame.to_csv(header=False,index=False,sep ='\t').encode('utf-8'),'interpolated_spectra.txt')
